9f677158447f8a2946f81a7e8403695bc7d33f4a
Oylsister <kkuiba2009@gmail.com>
2024-09-19 23:47:56 +0700

    making slider freeze when moving mouse over to it.

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -14,22 +14,52 @@ function Slider()
 {
     const [activeIndex, setActiveIndex] = useState(0);
     const [currentSlide, setCurrectSlide] = useState(infoSlider[0]);
+    const [slideDone, setSlideDone] = useState(true);
+    const [timeID, setTimeID] = useState(null);
 
-    setTimeout(() => {
-        if(activeIndex >= sildeImage.length - 1)
-            setActiveIndex(0);
+    useEffect(() => {
+        if(slideDone) {
+            setSlideDone(false);
+            setTimeID (
+                setTimeout(() => {
 
-        else
-            setActiveIndex(activeIndex + 1);
+                    //console.log(timeID);
 
-        let sliderBG = document.getElementById("sliderBG");
-        sliderBG.style.backgroundImage = `url("${sildeImage[activeIndex]}")`;
-        setCurrectSlide(infoSlider[activeIndex]);
+                    if(activeIndex >= sildeImage.length - 1)
+                        setActiveIndex(0);
+            
+                    else
+                        setActiveIndex(activeIndex + 1);
+            
+            
+                    let sliderBG = document.getElementById("sliderBG");
+                    sliderBG.style.backgroundImage = `url("${sildeImage[activeIndex]}")`;
+            
+                    setCurrectSlide(infoSlider[activeIndex]);
+                    setSlideDone(true);
+                }, 5000)
+            );
+        }
+    }, [slideDone]);
 
-    }, 5000);
+    const AutoPlayStop = () => {
+        console.log("They're Stop")
+        if (timeID > 0) {
+            clearTimeout(timeID);
+            setSlideDone(false);
+        }
+    };
+    
+      const AutoPlayStart = () => {
+        console.log("They're start")
+        if (!slideDone) {
+            setSlideDone(true);
+        }
+    };
 
     return (
-        <div className="slider-container" id="sliderBG">
+        <div className="slider-container" onMouseEnter={AutoPlayStop}
+        onMouseLeave={AutoPlayStart} id="sliderBG">
             {currentSlide}
         </div>
     );
@@ -38,7 +68,7 @@ function Slider()
 function ZombieSharpInfo()
 {
     return (
-        <div className='zombie-info'>
+        <div className='slider-info'>
             <div className='zombie-info-container'>
                 <h1 className='zombie-header'>ZombieSharp</h1>
                 <p className='zombie-para'>Bringing you to one of the most amazing gamemode of Counter-Strike known as <br /> "Zombie Mode". Now available for free!</p>
@@ -54,7 +84,7 @@ function ZombieSharpInfo()
 function JammingInfo()
 {
     return (
-        <div className='zombie-info'>
+        <div className='slider-info'>
             <div className='zombie-info-container'>
                 <h1 className='zombie-header'>Jamming</h1>
                 <p className='zombie-para'>An incredible app letting you to search and add song or music into your playlist with ease.</p>
