13b101325d42b32d2240c51489f06c4eadd6b693
James P. Naphat <kkuiba2009@gmail.com>
2024-09-23 16:28:36 +0700

    remove npm test for deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Intro from './components/intro/intro';
 import Skills from './components/skills/skills';
 import Slider from './components/slider/slider';
-import Header from './components/header/header';
-import MyWorks from './pages/myworks/myworks';
 
 function App() {
   return (
