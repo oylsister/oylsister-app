9dadc9893b490a125c7e12b03b818330cf832991
James P. Naphat <kkuiba2009@gmail.com>
2024-09-23 16:05:51 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import MyWorks from './pages/myworks/myworks';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "myworks",
+    element: <MyWorks />
+  }
+]);
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<RouterProvider router={router} />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
