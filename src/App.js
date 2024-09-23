a2b67a75d1915252c4cdca024d6ef640d4758751
James P. Naphat <kkuiba2009@gmail.com>
2024-09-23 16:23:43 +0700

    Added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Intro from './components/intro/intro';
 import Skills from './components/skills/skills';
 import Slider from './components/slider/slider';
+import Header from './components/header/header';
+import MyWorks from './pages/myworks/myworks';
 
 function App() {
   return (
-    <div className="App">
-      <Intro />
-      <Slider />
-      <Skills />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header/>}>
+          <Route index element={<App/>} />
+          <Route path="myworks" element={<MyWorks />} />
+        </Route>
+      </Routes>
+      <div className="App">
+        <Intro />
+        <Slider />
+        <Skills />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import MyWorks from './pages/myworks/myworks';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "myworks",
-    element: <MyWorks />
-  }
-]);
 
 test('renders learn react link', () => {
-  render(<RouterProvider router={router} />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
