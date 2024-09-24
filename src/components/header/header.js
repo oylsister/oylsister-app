06e6aefce3f9093c519302785cccc38d46bd294d
Oylsister <kkuiba2009@gmail.com>
2024-09-24 09:09:01 +0700

    Fix Dropdown Menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,10 +8,19 @@ import { Link } from "react-router-dom";
 function Header()
 {
     const [showInfo, setShowInfo] = useState(false);
+    const [smallMenu, setSmallMenu] = useState(false);
 
     const toggleModal = () => {
         setShowInfo(!showInfo);
     };
+
+    const showSmallMenu = () => {
+        setSmallMenu(true);
+    }
+
+    const stopSmallMenu = () => {
+        setSmallMenu(false);
+    }
 
     return (
         <div className="header">
@@ -23,14 +32,17 @@ function Header()
                     <button className="button-head"><Link to="/myworks">My Works</Link></button>
                     <button className="button-head" onClick={toggleModal}>Contract Me</button>
                 </div>
-                <div className="dropdown-container">
+                <div className="dropdown-container" onMouseEnter={showSmallMenu} onMouseLeave={stopSmallMenu}>
                     <button className="button-dropdown">Menu</button>
-                    <div className="dropdown-content">
-                        <button className="button-head">Home</button>
-                        <button className="button-head" onClick={Redirect.ZombieSharpLink}>Zombie Sharp</button>
-                        <button className="button-head">My Works</button>
-                        <button className="button-head" onClick={toggleModal}>Contract Me</button>
-                    </div>
+
+                    { smallMenu && (
+                        <div className="dropdown-content">
+                            <button className="button-head"><Link to="/">Home</Link></button>
+                            <button className="button-head" onClick={Redirect.ZombieSharpLink}>Zombie Sharp</button>
+                            <button className="button-head"><Link to="/myworks">My Works</Link></button>
+                            <button className="button-head" onClick={toggleModal}>Contract Me</button>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
